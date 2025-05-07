7ae73ba46f33a4aa47578a81ed871883dc5f7bc8
Phongzau <phongzau2004@gmail.com>
2025-05-07 21:07:14 +0700

    configuration router/layout

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import GlobalStyles from '@components/GlobalStyles/index.jsx';
+import { RouterProvider as Router } from 'react-router';
+import router from './routes';
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <StrictMode>
+        <GlobalStyles>
+            <Router router={router} />
+        </GlobalStyles>
+    </StrictMode>,
+);
---
+++ src/routes/index.jsx
@@ -0,0 +1,32 @@
+import { createBrowserRouter } from 'react-router-dom';
+import App from '../App';
+import Following from '../pages/Following';
+import Home from '../pages/Home';
+import Profile from '../pages/Profile';
+import { DefaultLayout } from '@components/Layout';
+import Upload from '../pages/Upload';
+import Search from '../pages/Search';
+
+function withLayout(Layout, Page) {
+    return (
+        <Layout>
+            <Page />
+        </Layout>
+    );
+}
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <App />,
+        children: [
+            { index: true, element: withLayout(DefaultLayout, Home) },
+            { path: '/following', element: withLayout(DefaultLayout, Following) },
+            { path: '/profile', element: withLayout(DefaultLayout, Profile) },
+            { path: '/upload', element: <Upload /> },
+            { path: '/search', element: <Search /> },
+        ],
+    },
+]);
+
+export default router;
