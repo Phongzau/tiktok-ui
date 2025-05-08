cdadc6bf9f4100ce89d9b44b2642862071d9cf54
Phongzau <phongzau2004@gmail.com>
2025-05-08 21:06:41 +0700

    add fontawesome + classnames and build ui header

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@components': path.resolve(__dirname, 'src/components'),
-      '@utils': path.resolve(__dirname, './src'), // alias cho myAlert.jsx nằm trong src
+    plugins: [react()],
+    resolve: {
+        alias: {
+            '@components': path.resolve(__dirname, 'src/components'),
+            '@utils': path.resolve(__dirname, './src'), // alias cho myAlert.jsx nằm trong src
+            '@assets': path.resolve(__dirname, 'src/assets'),
+        },
     },
-  },
-})
+});
