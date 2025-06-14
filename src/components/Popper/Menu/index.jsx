f55ff1d5baa444a5623a2a57578e97f338bdb3db
Phongzau <phongzau2004@gmail.com>
2025-06-14 14:51:31 +0700

    add library PropTypes, edit some file

--- src/components/Popper/Menu/index.jsx
+++ src/components/Popper/Menu/index.jsx
@@ -6,6 +6,7 @@ import Header from './Header';
 import MenuItem from './MenuItem';
 import styles from './Menu.module.scss';
 import { useState } from 'react';
+import PropTypes from 'prop-types';
 
 const cx = classNames.bind(styles);
 
@@ -47,7 +48,7 @@ function Menu({ children, items = [], hideOnClick = false, onChange = defaultFn 
                     <PopperWrapper className={cx('menu-popper')}>
                         {history.length > 1 && (
                             <Header
-                                title="Language"
+                                title={current.title}
                                 onBack={() => setHistory((prev) => prev.slice(0, prev.length - 1))}
                             />
                         )}
@@ -61,5 +62,10 @@ function Menu({ children, items = [], hideOnClick = false, onChange = defaultFn 
         </Tippy>
     );
 }
-
+Menu.propTypes = {
+    children: PropTypes.node.isRequired,
+    items: PropTypes.array,
+    hideOnClick: PropTypes.bool,
+    onChange: PropTypes.func,
+};
 export default Menu;
--- src/layouts/DefaultLayout/Header/index.jsx
+++ src/layouts/DefaultLayout/Header/index.jsx
@@ -102,34 +102,6 @@ function Header() {
                     <img src={images.logo} alt="Tiktok" />
                 </Link>
 
-                {/* <HeadlessTippy
-                    interactive={true}
-                    visible={searchResult.length > 0}
-                    render={(attrs) => (
-                        <div className={cx('search-result')} tabIndex="-1" {...attrs}>
-                            <PopperWrapper>
-                                <h4 className={cx('search-title')}>Accounts</h4>
-                                <AccountItem />
-                                <AccountItem />
-                                <AccountItem />
-                                <AccountItem />
-                            </PopperWrapper>
-                        </div>
-                    )}
-                >
-                    <div className={cx('search')}>
-                        <input placeholder="Search accounts and videos" spellCheck={false} />
-                        <button className={cx('clear')}>
-                            <FontAwesomeIcon icon={faCircleXmark} />
-                        </button>
-                        <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
-                        <button className={cx('search-btn')}>
-                            <SearchIcon />
-                        </button>
-                    </div>
-                </HeadlessTippy> */}
-
-                {/* Search */}
                 <Search />
 
                 <div className={cx('actions')}>
